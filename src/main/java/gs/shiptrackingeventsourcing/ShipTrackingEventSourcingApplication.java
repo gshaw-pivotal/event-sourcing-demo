3ae2f96495e5cc9a13ae370c3b2b9b03856af186
Gavin Shaw <gshaw+github@pivotal.io>
2022-05-20 14:15:36 -0400

    Initial commit

---
+++ src/main/java/gs/shiptrackingeventsourcing/ShipTrackingEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package gs.shiptrackingeventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipTrackingEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipTrackingEventSourcingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gs/shiptrackingeventsourcing/ShipTrackingEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package gs.shiptrackingeventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipTrackingEventSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
